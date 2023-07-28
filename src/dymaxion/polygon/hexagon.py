d7befb11df9e5c68be2dca6cbace57bedf6e9b53
Lars van Vianen <lars@vianen.com>
2023-07-28 17:54:08 +0200

    Update hexagon.py

--- src/dymaxion/polygon/hexagon.py
+++ src/dymaxion/polygon/hexagon.py
@@ -1,14 +1,19 @@
 
 import math
 
+
 class Hexagon(Polygon):
 
     def __init__(self, side_length):
+        """
+        """
         super().__init__(6, side_length)
         self.s = side_length
 
     def area(self):
-        """Return the area of the hexagon."""
+        """
+        Return the area of the hexagon.
+        """
         return (3 * math.sqrt(3) * self.s ** 2) / 2
 
     def circumradius(self):
